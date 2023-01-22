999209c792d09dd8ca70d6c56e5c483380697a6c
Jongseo Lee <james0614ll@gmail.com>
2023-01-22 14:39:23 +0000

    complete weather

--- chrome/js/weather.js
+++ chrome/js/weather.js
@@ -3,8 +3,18 @@ const API_KEY = "030585c4f05b24b8630a22a7141b4371";
 function geoSuccess(location) {
     const lat = location.coords.latitude;
     const lon = location.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-    fetch(url);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            const temperature = data.main.temp;
+            const description =  data.weather[0].main;
+            weather.innerText = temperature + '°C ' + description;
+        });
+    //console.log(res);
 };
 
 function geoError() {
